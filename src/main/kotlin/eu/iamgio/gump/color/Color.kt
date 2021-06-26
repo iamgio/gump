0020a375b494529a5aa544c2f33ff5f8a27b5fa7
Giorgio Garofalo <giogar2705@gmail.com>
2021-06-26 15:30:39 +0200

    Add Color.hex and .hsb

--- src/main/kotlin/eu/iamgio/gump/color/Color.kt
+++ src/main/kotlin/eu/iamgio/gump/color/Color.kt
@@ -1,7 +1,7 @@
 package eu.iamgio.gump.color
 
 /**
- * An RGBA color.
+ * An RGBA color. R, G and B are 0-255, alpha is 0-1.
  *
  * @author Giorgio Garofalo
  */
@@ -16,6 +16,20 @@ data class Color(val red: Int, val green: Int, val blue: Int, val alpha: Double 
          * @return AWT color to Gump Color
          */
         fun fromAWT(color: java.awt.Color) = Color(color.red, color.green, color.blue)
+
+        /**
+         * @param hue 0-1 hue value
+         * @param saturation 0-1 saturation value
+         * @param brightness 0-1 brightness value
+         * @return color from HSB
+         */
+        fun hsb(hue: Float, saturation: Float, brightness: Float) = java.awt.Color.getHSBColor(hue, saturation, brightness).toColor()
+
+        /**
+         * @param hex hexadecimal color value, with or without #
+         * @return decoded color from hex
+         */
+        fun hex(hex: String) = java.awt.Color.decode(if(hex.startsWith("#")) hex else "#$hex").toColor()
     }
 }
 
--- src/test/kotlin/eu/iamgio/gump/test/GumpTest.kt
+++ src/test/kotlin/eu/iamgio/gump/test/GumpTest.kt
@@ -2,7 +2,7 @@ package eu.iamgio.gump.test
 
 import eu.iamgio.gump.app.AppSettings
 import eu.iamgio.gump.app.GumpApplication
-import eu.iamgio.gump.color.toColor
+import eu.iamgio.gump.color.Color
 import eu.iamgio.gump.component.Component
 import eu.iamgio.gump.component.shape.Rectangle
 
@@ -14,7 +14,7 @@ class GumpTest : GumpApplication() {
     override fun createSettings() = AppSettings()
 
     override fun buildRoot(): Component {
-        return Rectangle(100.0, 50.0, java.awt.Color.BLUE.toColor())
+        return Rectangle(100.0, 50.0, Color.hsb(super.age / 200F, 1F, 1F))
     }
 }
 
